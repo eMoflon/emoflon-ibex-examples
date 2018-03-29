5944373fd483c14f02897663557987db87a0cdbf
Anthony Anjorin <tonyanjorin@googlemail.com>
2018-03-29 23:38:41 +0200

    Reverted to state for first handbook

--- sokoban/SokobanGUI/src/org/moflon/tutorial/sokobangamegui/rules/SokobanRules.java
+++ sokoban/SokobanGUI/src/org/moflon/tutorial/sokobangamegui/rules/SokobanRules.java
@@ -1,20 +1,14 @@
 package org.moflon.tutorial.sokobangamegui.rules;
 
-import org.emoflon.tutorial.sokobanrules.SokobanValidator;
-import org.emoflon.tutorial.sokobanrules.api.SokobanrulesAPI;
-
 import SokobanLanguage.Board;
 import SokobanLanguage.Field;
 import SokobanLanguage.Figure;
 
 public class SokobanRules {
-	private SokobanValidator validator;
-	private SokobanrulesAPI api;
 	private String allsWell = "Everything seems to be ok...";
 
 	public SokobanRules(Board board) {
-		validator = new SokobanValidator(board);
-		api = validator.getAPI();
+
 	}
 
 	public Result move(Figure figure, Field field) {
@@ -24,26 +18,7 @@ public class SokobanRules {
 	}
 
 	public Result validateBoard(Board board) {
-		if (!api.atLeatOneSokoban().hasMatches())
-			return new Result(false, "You need to have at least one Sokoban!");
-
-		if (api.moreThanOneSokoban().hasMatches())
-			return new Result(false, "You cannot have more than one Sokoban!");
-
-		if (!api.atLeastOneBlock().hasMatches())
-			return new Result(false, "You must have at least one block!");
-
-		if (!api.atLeastOneEndField().hasMatches())
-			return new Result(false, "You must have at least one end field!");
-
-		if (api.atLeastOneBlock().countMatches() != api.atLeastOneEndField().countMatches())
-			return new Result(false, "You must have exactly as many end fields as blocks!");
-
-		return api.aTargetFieldIsOccupied()//
-				.findAnyMatch()//
-				.map(m -> m.getField())//
-				.map(f -> new Result(false, "Field [" + f.getRow() + ", " + f.getCol() + "] " + //
-						"is an end position but is occupied!"))//
-				.orElse(new Result(true, allsWell));
+		// FIXME: Check if the board is valid
+		return new Result(true, allsWell);
 	}
 }
