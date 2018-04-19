14454cd86b851a6d0be91b6e8c9bc603414ed28e
Anthony Anjorin <tonyanjorin@googlemail.com>
2018-04-19 22:39:21 +0200

    Trying to fix problem with file separator char

--- sokoban/SokobanRules/src/SokobanRules/SokobanValidator.java
+++ sokoban/SokobanRules/src/SokobanRules/SokobanValidator.java
@@ -11,7 +11,7 @@ import SokobanRules.api.SokobanRulesDemoclesApp;
 public class SokobanValidator extends SokobanRulesDemoclesApp {
 	public SokobanValidator(Board board) {
 		File root = new File(SokobanValidator.class.getResource(".").getFile());
-		workspacePath = root.getParentFile().getParentFile().getParent() + "/";
+		workspacePath = root.getParentFile().getParentFile().getParent() + File.separatorChar;
 
 		createModel(URI.createURI("board.xmi"));
 		resourceSet.getResources().get(0).getContents().add(board);
