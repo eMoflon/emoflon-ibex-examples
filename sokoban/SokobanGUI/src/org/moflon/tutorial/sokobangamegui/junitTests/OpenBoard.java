61194bdf07bee5efa6d9a215c49c52648472250a
surendra2016 <surendra@mail.uni-paderborn.de>
2018-07-04 02:00:53 +0200

    adding test case to validate single figure on the board

--- sokoban/SokobanGUI/src/org/moflon/tutorial/sokobangamegui/junitTests/OpenBoard.java
+++ sokoban/SokobanGUI/src/org/moflon/tutorial/sokobangamegui/junitTests/OpenBoard.java
@@ -8,20 +8,40 @@ import java.awt.Robot;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.eclipse.emf.ecore.EClass;
 import org.junit.jupiter.api.Test;
 import org.moflon.tutorial.sokobangamegui.controller.BoardCreator;
 import org.moflon.tutorial.sokobangamegui.controller.Controller;
+import org.moflon.tutorial.sokobangamegui.rules.SokobanRules;
 import org.moflon.tutorial.sokobangamegui.tests.AutoClick;
+
+import SokobanLanguage.Board;
+import SokobanLanguage.Field;
+import SokobanLanguage.Figure;
+import SokobanLanguage.SokobanLanguageFactory;
 
 class OpenBoard {
 	private Robot robot;
 	//private AutoClick autoclick;
 	private Runtime runtime;
-	
-	
+	private Board board;
+	private SokobanRules sokobanRules;
 
 	@Test
-	void test() {
+	void test_launch_empty_board() {
+		Logger.getRootLogger().setLevel(Level.DEBUG);
+		/* Create an instance of this class and create an empty board */
+		Controller controller = new Controller();
+		board = BoardCreator.createEmptyBoard(8, 8);
+		controller.switchBoard(board);
+		
+		//robot.delay(2000); // give the delay. Robot does not sleep. that's why we have to provide delay
+		
+		//robot.mouseMove(arg0, arg1);
+		assertEquals("Welcome to Sokoban!", controller.currentStatus);
+	}
+	@Test
+	void test_add_sokoban() {
 		Logger.getRootLogger().setLevel(Level.DEBUG);
 		/*runtime = Runtime.getRuntime();
 		
@@ -35,12 +55,35 @@ class OpenBoard {
 
 		/* Create an instance of this class and create an empty board */
 		Controller controller = new Controller();
-		controller.switchBoard(BoardCreator.createEmptyBoard(8, 8));
+		board = BoardCreator.createEmptyBoard(8, 8);
+		controller.switchBoard(board);
 		
 		//robot.delay(2000); // give the delay. Robot does not sleep. that's why we have to provide delay
 		
 		//robot.mouseMove(arg0, arg1);
 		assertEquals("Welcome to Sokoban!", controller.currentStatus);
+		
+		for (EClass elementClass : controller.getFigureClasses()) {
+			//ec = elementClass;
+			if (elementClass.getName() == "Sokoban") {
+				System.out.println("You have got the sokoban..!!");
+				final EClass ec = elementClass; 
+				for (Field f : board.getFields()) {
+					System.out.println(f.getRow());
+					Figure newFigure = (Figure) SokobanLanguageFactory.eINSTANCE.create(ec);
+					controller.setFigure(f, newFigure);
+					sokobanRules = new SokobanRules(board);
+					try {
+						Thread.sleep(5000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					break;
+				}
+			} 
+		}
+		
 	}
 
 }
