d8a4a34465110de34aaedce3fbc8ac03c3ff2f05
surendra2016 <surendra@mail.uni-paderborn.de>
2018-08-02 13:44:49 +0200

    classifying the test cases among separate classes like validations, sokoban movement and file import export

--- sokoban/TestSuiteSokobanGUI/src/org/moflon/tutorial/sokobangui/tests/TestSokobanBoardValidation.java
+++ sokoban/TestSuiteSokobanGUI/src/org/moflon/tutorial/sokobangui/tests/TestSokobanBoardValidation.java
@@ -4,18 +4,12 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.io.File;
-
-import org.eclipse.emf.common.util.URI;
-import org.junit.Before;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.moflon.tutorial.sokobangamegui.controller.BoardCreator;
 import org.moflon.tutorial.sokobangamegui.controller.Controller;
-import org.moflon.tutorial.sokobangamegui.view.actions.PlayAction;
 
-class TestBoardCreation {
+class TestSokobanBoardValidation {
 
 	private TestView view;
 	private Controller controller;
@@ -28,11 +22,6 @@ class TestBoardCreation {
 		});
 		controller.switchBoard(BoardCreator.createEmptyBoard(8, 8));
 	}
-	
-	/*@AfterEach
-	public void deleteView() {
-		controller = null;
-	}*/
 	
 	@Test
 	public void testSingleSokoban() {
@@ -196,96 +185,6 @@ class TestBoardCreation {
 		assertFalse(controller.boardIsValid());
 	}
 	
-	@Test
-	public void testValidBoardMoveSokoban() {
-		view.createSokoban(2,2);
-		view.createBlock(5,6);
-		view.createBoulder(3,2);
-		view.createEndPos(2,5);
-		assertEquals(ExpectedBoards.validBoard(), view.printBoard());
-		assertTrue(controller.boardIsValid());
-		view.setPlayModus(true);
-		view.moveFigure(view.getField(2,2), view.getField(2,3));
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	@Test
-	public void testValidBoardMoveBlock() {
-		view.createSokoban(2,2);
-		view.createBlock(2,3);
-		view.createBoulder(3,2);
-		view.createEndPos(2,5);
-		assertEquals(ExpectedBoards.validBoardMoveBlock(), view.printBoard());
-		assertTrue(controller.boardIsValid());
-		view.setPlayModus(true);
-		view.moveFigure(view.getField(2,2), view.getField(2,3));
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		view.moveFigure(view.getField(2,3), view.getField(2,4));
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		assertEquals(ExpectedBoards.validBoardMoveBlockAfterMove(), view.printBoard());
-	}
-	
-	@Test
-	public void testValidBoardMoveBoulder() {
-		view.createSokoban(2,2);
-		view.createBlock(3,2);
-		view.createBoulder(2,3);
-		view.createEndPos(2,5);
-		assertEquals(ExpectedBoards.validBoardMoveBoulder(), view.printBoard());
-		assertTrue(controller.boardIsValid());
-		view.setPlayModus(true);
-		view.moveFigure(view.getField(2,2), view.getField(2,3));
-		//view.moveFigure(view.getField(2,3), view.getField(2,4));
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		assertEquals(ExpectedBoards.validBoardMoveBoulder(), view.printBoard());
-	}
-	
-	@Test
-	public void testValidBoardImportFile() {
-		controller.loadSOKFile("boards/chaos.sok");
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e){
-			e.printStackTrace();
-		}
-		assertEquals(ExpectedBoards.validBoardFileImport(), view.printBoard());
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	@Test
-	public void testValidBoardExportFile() {
-		view.createSokoban(2,2);
-		view.createBlock(5,6);
-		view.createBoulder(3,2);
-		view.createEndPos(2,5);
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		
-		controller.saveSOKFile("boards/gen_board.sok");
-	}
 	@Test
 	public void testValidBoardClearBoardAction() {
 		view.createSokoban(2,2);
