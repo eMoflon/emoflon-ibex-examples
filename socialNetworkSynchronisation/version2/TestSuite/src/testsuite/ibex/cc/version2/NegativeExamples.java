ed40fbc8816655cb28c7dc1c989c841d8a786043
Anthony Anjorin <tonyanjorin@googlemail.com>
2019-01-26 19:33:33 +0100

    Removed redundant test, formatting

--- socialNetworkSynchronisation/version2/TestSuite/src/testsuite/ibex/cc/version2/NegativeExamples.java
+++ socialNetworkSynchronisation/version2/TestSuite/src/testsuite/ibex/cc/version2/NegativeExamples.java
@@ -22,42 +22,50 @@ public class NegativeExamples extends CCTestCase {
 		runCC();
 		Assert.assertFalse(checker.modelsAreConsistent());
 	}
+
 	@Test
 	public void testForIgnoreParentRelations() throws IOException {
 		testForPairOfModels("version2/users/TwoFacebbookOtherUsers", "version2/users/OneInstagramOtherUser");
 	}
+
 	@Test
 	public void testForIgnoreSiblingWithFriendsRelations() throws IOException {
 		testForPairOfModels("version2/users/FacebookSiblingRelationOtherUsers", "version2/users/ThreeInstagramUsers");
 	}
+
 	@Test
 	public void testForIgnoreSpouseRelations() throws IOException {
 		testForPairOfModels("version2/users/FacebookSpouseRelationUsers", "version2/users/ThreeInstagramUsers");
 	}
+
 	@Test
 	public void testForIgnoreSpouseBidirectionRelations() throws IOException {
 		testForPairOfModels("version2/users/FacebookSpouseRelationBidirectionRelation",
 				"version2/users/ThreeInstagramUsers");
 	}
+
 	@Test
 	public void testForIgnoreInterNetworkFollowership() throws IOException {
 		testForPairOfModels("version2/networks/FacebookFriendsNetworkOfNetwork",
 				"version2/networks/InstagramOtherNetworkOfNetworks");
 	}
-  @Test
+
+	@Test
 	public void testForWrongIgnoreRelations() throws IOException {
 		testForPairOfModels("version2/users/FacebookWithParentRelation", "version2/users/OneInstagramUser");
 	}
-  @Test
+
+	@Test
 	public void testForWrongInterNetworkFollowshipRelations() throws IOException {
 		testForPairOfModels("version2/networks/OneFacebookMultipleNetwork",
 				"version2/networks/OneInstagramMultipleUserNetwork");
 	}
+
 	@Test
 	public void testForHandleIntraNetworkFollowers() throws IOException {
-		
 		testForPairOfModels("version2/users/TwoFacebookUsers", "version2/users/OneInstagramUser");
 	}
+
 	@Test
 	public void testForHandleIntraNetworkFollowers1() throws IOException {
 		testForPairOfModels("version2/networks/OneFacebookNetwork", "version2/networks/TwoInstagramNetwork");
